695fd695147cec5145b03d6e7f2625b29f30728a
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-05-04 13:19:30 +0530

    initial

---
+++ Backend/Models/User.model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const UserSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    age: Number,
+})
+const UserModel = mongoose.model("users", UserSchema)
+module.exports = UserModel;
---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+
+const UserRouter = require("./Routes/User.route");
+const connection = require("./Config/db");
+const express = require("express");
+const cors = require("cors")
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+
+app.use("/users", UserRouter)
+
+
+app.get("/", (req, res) => {
+    try {
+        res.status(200).json({ Message: "Welcome to Chat Application" });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ Error: err })
+    }
+});
+
+app.listen(process.env.PORT, async () => {
+    try {
+        await connection;
+        console.log("Connected to DB");
+    } catch (err) {
+        console.log("Error connecting to DB");
+    }
+    console.log(`Server is Rocking on port ${process.env.PORT}`);
+});
